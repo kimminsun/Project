df852650d9c542bcc7610bf5df37ad7ee03b99ad
Minsun Kim <minsun2767@naver.com>
2016-10-31 21:13:16 +0900

    #1 add splash

---
+++ app/src/main/java/kr/hs/emrim/minsun2767/project/SplashActivity.java
@@ -0,0 +1,29 @@
+package kr.hs.emrim.minsun2767.project;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+
+    }
+
+}
